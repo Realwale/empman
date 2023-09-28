b9fb3b94da4295ea8f03aecbde19ebae5e5ae339
Realwale <olawaleagboola41@gmail.com>
2023-09-28 18:02:20 +0100

    Eureka server configured for service registry

--- employee-service/src/main/java/org/waletech/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/org/waletech/employeeservice/EmployeeServiceApplication.java
@@ -2,35 +2,12 @@ package org.waletech.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+
+
 
 @SpringBootApplication
-@EnableFeignClients
 public class EmployeeServiceApplication {
 
-	/*
-	1. Configuring RestTemplate bean for synchronous communication
-
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
-
-	 */
-
-
-	/*
-	2. Configuring WebClient bean for synchronous communication
-
-	@Bean
-	public WebClient webClient(){
-		return WebClient.builder().build();
-	}
-
-	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
